5b4804687448c8c1647cd5be51a025018c6c5111
viloo <victor.lhomme@orange.fr>
2025-01-31 20:26:58 +0100

    Création du projet : Activité principale / Activité Scénario

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.doyourjdr"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.doyourjdr"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    /*
+    composeOptions {
+        kotlinCompilerExtensionVersion "1.5.2"
+    }
+
+     */
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
+    // A GARDER
+    implementation(libs.constraintlayout.compose)
+
+
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/doyourjdr/vue/MainActivity.kt
@@ -0,0 +1,323 @@
+package com.example.doyourjdr.vue
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.doyourjdr.ui.theme.DoYourJDRTheme
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.doyourjdr.R
+import com.example.doyourjdr.vue.scenario.Scenario
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("JE SUIS A LA PREMIERE ACTIVITE")
+        enableEdgeToEdge()
+        setContent {
+            ConstructionComposant()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ConstructionComposant() {
+    DoYourJDRTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AfficheImageFond()
+            AfficheBouton()
+        }
+    }
+}
+
+@Composable
+fun AfficheBouton() {
+    val modifierBorder = Modifier.border(2.dp, Color(0x016D490B))
+    val background = Modifier.background(Color.DarkGray)
+    val modifierBackground = Modifier.background(Color(0xAC505050))
+    val topTitre = 0.15f
+    val fontSizeLittle = 18
+    val fontSizeTitle = 22
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (titre, scenario, continuer, carte, personnage) = createRefs()
+        val topLimite = createGuidelineFromTop(topTitre)
+        val midLimite = createGuidelineFromTop(topTitre + 0.27f)
+        val startLimite = createGuidelineFromStart(-0.5f)
+        val endLimite = createGuidelineFromStart(0.5f)
+
+        ContainerBox(
+            modifier = Modifier
+                .fillMaxWidth(0.42f)
+                .aspectRatio(1f)
+                .constrainAs(titre) {
+                    top.linkTo(topLimite)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            composant = {
+                RotatedTitleWithText(
+                    "DoYourJdr",
+                    modifierBorder.then(background),
+                    fontSizeTitle
+                )
+            },
+        )
+
+        ContainerBox(
+            modifier = Modifier
+                .fillMaxWidth(0.29f)
+                .aspectRatio(1f)
+                .constrainAs(scenario) {
+                    top.linkTo(midLimite)
+                    start.linkTo(startLimite)
+                    end.linkTo(parent.end)
+                },
+            composant = {
+                RotatedBoxWithText(
+                    titre ="Scénario",
+                    modifier = modifierBorder.then(modifierBackground),
+                    fontSize = fontSizeLittle,
+                    route = Scenario::class.java
+                )
+            }
+        )
+        ContainerBox(
+            modifier = Modifier
+                .fillMaxWidth(0.29f)
+                .aspectRatio(1f)
+                .constrainAs(carte) {
+                    top.linkTo(midLimite)
+                    start.linkTo(endLimite)
+                    end.linkTo(parent.end)
+                },
+            composant = {
+                RotatedBoxWithText(
+                    "Carte",
+                    modifierBorder.then(modifierBackground),
+                    fontSizeLittle
+                )
+            }
+        )
+        ContainerBox(
+            modifier = Modifier
+                .fillMaxWidth(0.38f)
+                .aspectRatio(1f)
+                .constrainAs(continuer) {
+                    top.linkTo(carte.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            composant = {
+                RotatedBoxWithText(
+                    "Continuer la partie",
+                    modifierBorder.then(modifierBackground),
+                    fontSizeTitle
+                )
+            }
+        )
+        ContainerBox(
+            modifier = Modifier
+                .fillMaxWidth(0.29f)
+                .aspectRatio(1f)
+                .constrainAs(personnage) {
+                    top.linkTo(continuer.bottom)
+                    start.linkTo(startLimite)
+                    end.linkTo(parent.end)
+                },
+            composant = {
+                RotatedBoxWithText(
+                    "Personnage",
+                    modifierBorder.then(modifierBackground),
+                    fontSizeLittle,
+
+                )
+            }
+        )
+    }
+}
+
+@Composable
+private fun ContainerBox(
+    modifier: Modifier,
+    composant: @Composable () -> Unit,
+) {
+    Box(modifier = modifier) {
+        composant()
+    }
+}
+
+@Composable
+private fun RotatedTitleWithText(titre: String, modifier: Modifier, fontSize: Int) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+
+            .graphicsLayer { rotationZ = 45f }
+            .then(modifier),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.muraille),
+            contentDescription = "Une image depuis drawable",
+            modifier = Modifier
+                .height(45.dp)
+                .fillMaxWidth()
+                .offset(y = (-35).dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Image(
+            painter = painterResource(id = R.drawable.muraille),
+            contentDescription = "Une image depuis drawable",
+            modifier = Modifier
+                .height(45.dp)
+                .fillMaxWidth()
+                .graphicsLayer { rotationZ = -90f }
+                .align(Alignment.CenterStart)
+                .offset(y = -90.dp),
+            contentScale = ContentScale.FillWidth
+
+        )
+        Column(modifier = Modifier
+            .fillMaxWidth(1f)
+            .graphicsLayer { rotationZ = -45f }
+            .aspectRatio(1f)) {
+            Column(modifier = Modifier.fillMaxHeight(0.55f)) {
+                Box(modifier = Modifier
+                    .fillMaxHeight(0.4f)
+                    .fillMaxWidth())
+                Text(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                    text = "8",
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontSize = (fontSize * 1.5).sp
+                )
+            }
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                thickness = 2.dp,
+                color = Color.Red,
+
+            )
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier
+                    .fillMaxHeight(0.1f)
+                    .fillMaxWidth())
+                Text(
+                    text = titre,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = fontSize.sp
+                )
+            }
+
+        }
+    }
+}
+
+
+@Composable
+private fun RotatedBoxWithText(titre: String, modifier: Modifier, fontSize: Int, route: Class<out Activity>? = null) {
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier
+            .graphicsLayer { rotationZ = 45f }
+            .border(5.dp, Color(0xFF6D490B))
+            .then(modifier)
+            .clickable {
+                // Action quand on clique sur la Box
+                if (route != null) {
+                    val intent = Intent(context, route)
+                    context.startActivity(intent)
+                }
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center)
+                .then(modifier),
+        ) {
+            Text(
+                text = titre,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .graphicsLayer { rotationZ = -45f }
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = fontSize.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun AfficheImageFond() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.systemBars.asPaddingValues())
+            .clipToBounds(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fondecran),
+            contentDescription = "Une image depuis drawable",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    scaleX = 1.7f
+                    scaleY = 1.7f
+                }
+        )
+    }
+}
+
